02936bfc8662618b04c4b2da22f7a4ecaf3e635a
Ben Boyter <ben@boyter.org>
2023-12-19 14:03:28 +1100

    start on cleanup script

--- check.py
+++ check.py
@@ -1,12 +1,24 @@
 import urllib.request
 import json
 import time
+import ssl
 
 def ping_url(url):
     try:
-        urllib.request.urlopen(url)
-        print(f"Successfully pinged {url}")
-        return True
+        # Disable SSL/TLS certificate verification
+        context = ssl._create_unverified_context()
+
+        # Fake being Windows Firefox to avoid most bot detection
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'}
+
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req, timeout=10, context=context) as response:
+            http_response_code = response.getcode()
+            if http_response_code >= 200 or http_response_code <= 299:
+                print(f"Successfully pinged {url}")
+                return True
+            print(f"Unsuccessfully pinged {url} - {http_response_code}")
+        return False
     except urllib.error.URLError as e:
         print(f"Failed to ping {url}. Error: {e}")
         return False
@@ -20,8 +32,12 @@ with open('feeds.json', 'r') as file:
     file_content = file.read()
     json_data = json.loads(file_content)
 
+found = []
 for x in json_data:
-
+    if x in found:
+        print(f"Duplicate {x}")
+        continue
+    found.append(x)
     if ping_url(x['url']):
         x['failCount'] = 0
     else:
@@ -35,4 +51,4 @@ json_dump = json.dumps(json_data)
 
 # save it out
 with open('feeds2.json', 'w') as file:
-    file.write()
+    file.write(json_dump)
